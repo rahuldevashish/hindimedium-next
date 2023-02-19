9f9514da53513f6a6a5e905c3eabe221a980f337
rahuldevashish <rahuldevashish@gmail.com>
2023-02-19 11:54:23 +0530

    edits document file

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-
-
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head />
         <body>
+         
+          <Main />
+          <NextScript />
           <p> Hare Krishna </p>
         </body>
       </Html>
     )
   }
 }
-
 export default MyDocument
